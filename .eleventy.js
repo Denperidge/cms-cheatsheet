b027cd6311dd4ca31a7987a5b0e09850266c31a6
Denperidge <cat@denperidge.com>
2024-11-25 22:56:03 +0100

    Updated styling, added layout.pug

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
-import { readFileSync } from "fs"; 
-
-const COLLECTION_NAME = "DenperidgeCheatsheet";
 
 export const config = {
     dir: {
@@ -10,14 +7,7 @@ export const config = {
     }
 };
 
-function _readJson(filename) {
-    return JSON.parse(readFileSync(`_data/${filename}   `, {encoding: "utf-8"}))
-}
-
-function getCheatsheet() {
-    _readJson("_data/")
-}
-
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(pugPlugin);
+    eleventyConfig.addPassthroughCopy("node_modules/@picocss/pico/css/pico.cyan.min.css")
 }
