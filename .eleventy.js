b6e854299eac20d814f361091fdb50a0ed39e4c7
Denperidge <cat@denperidge.com>
2024-11-26 14:52:05 +0100

    Implemented auto-cache-buster, moved styling to css, fixed small screen styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 import pugPlugin from "@11ty/eleventy-plugin-pug";
+import eleventyAutoCacheBuster from "eleventy-auto-cache-buster";
+
 const {default: cheatsheet} = await import("./src/_data/DenperidgeCheatsheet.json", {assert: {type: "json"}})
 
 export const config = {
@@ -10,7 +12,9 @@ export const config = {
 
 export default function (eleventyConfig) {
     eleventyConfig.addPlugin(pugPlugin);
+    eleventyConfig.addPlugin(eleventyAutoCacheBuster);
     eleventyConfig.addPassthroughCopy("node_modules/@picocss/pico/css/pico.cyan.min.css")
+    eleventyConfig.addPassthroughCopy("src/static/")
 
     const tagSet = new Set();
     cheatsheet.forEach(entry => {
