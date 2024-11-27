48d8630444a7863264aba4092992484b62ba61b4
Denperidge <cat@denperidge.com>
2024-11-27 14:28:07 +0100

    Functional sorting!

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("node_modules/prismjs/themes/prism-coy.min.css")
     eleventyConfig.addPassthroughCopy("node_modules/jquery/dist/jquery.min.js")
     eleventyConfig.addPassthroughCopy("node_modules/fastest-levenshtein/esm/mod.js")
+    eleventyConfig.addPassthroughCopy("node_modules/string-comparison/dist/index.mjs")
     eleventyConfig.addPassthroughCopy("src/static/")
 
     const tagSet = new Set();
--- src/static/sheet.js
+++ src/static/sheet.js
@@ -1,29 +1,49 @@
 import { closest, distance} from "/node_modules/fastest-levenshtein/esm/mod.js";
+
+import stringComparison from "/node_modules/string-comparison/dist/index.mjs";
+let cosine = stringComparison.cosine
+console.log(cosine)
+
 let cache = [];
-function cacheEntries() {
+async function cacheEntries() {
     $(".entry").each((i, entry) => {
         entry = $(entry);
         //cache.push(`${entry.text()}//${entry.attr("id")}`)
-        cache.push(`${entry.children("header").text()}`)
+        cache.push(`${entry.children("header").text()}//${entry.attr("id")}`)
     })
     console.log(cache)
 }
 const search = $("#search")
 
-function searchEntries() {
+async function searchEntries() {
     
-    console.log(search.val());
-    console.log("---")
+    /*
     cache.forEach((cacheEntry) => {
         console.log(cacheEntry, distance(search.val(), cacheEntry))
-    })
+    })*/
     //const res = closest(search.val(), cache)
-    console.log(res)
+    
+    console.log(search.val())
+    let matches = cosine.sortMatch(search.val(), cache);
+    matches = matches.sort(match => match.rating).map(match => match.member.substring(match.member.lastIndexOf("//") + 2))
+    let sortedChildren = $("#sheet").children(".entryLi").sort(function(a,b) {
+        const aIndex = matches.indexOf(a.querySelector("article").id);
+        const bIndex = matches.indexOf(b.querySelector("article").id);
+        if (aIndex < bIndex) {
+            return -1;
+        } else if (aIndex > bIndex) {
+            return 1;
+        } else {
+            return 0;
+        }
+    })
+    $("#sheet").append(sortedChildren)
+    console.log(sortedChildren)
 }
 
-search.on("focus", function() {
+search.on("focus", async function() {
     if (cache.length <= 0) {
-        cacheEntries()
+        await cacheEntries()
     }
-    search.on("keydown", searchEntries);
+    search.on("keyup", searchEntries);
 });
